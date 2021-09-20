d040246c71564d9be5b85d5c0252166827e92154
Rohit Bisht <bishtrohit097@gmail.com>
2021-09-20 17:25:19 +0530

    code organised

--- or.py
+++ or.py
@@ -2,6 +2,7 @@ from utils.model import Perceptron
 from utils.all_util import prepare_data, save_plot,save_model
 import pandas as pd
 import numpy as np
+
 def main(data,eta,epochs,filename,plotFileName):
 
     df = pd.DataFrame(data)
@@ -16,11 +17,13 @@ def main(data,eta,epochs,filename,plotFileName):
     save_model(model,filename=filename)
     save_plot(df,plotFileName ,model)
 if __name__=="__main__":
-    AND = {
-    "x1": [0,0,1,1],
-    "x2": [0,1,0,1],
-    "y": [0,0,0,1],
+    OR = {
+        "x1": [0,0,1,1],
+        "x2": [0,1,0,1],
+        "y": [0,1,1,1],
     }
+
     ETA = 0.3 # 0 and 1
     EPOCHS = 10
-    main(data=AND,eta=ETA,epochs=EPOCHS,filename="and.model",plotFileName="and.png")
+    main(data=OR,eta=ETA,epochs=EPOCHS,filename="or.model",plotFileName="or.png")
+
